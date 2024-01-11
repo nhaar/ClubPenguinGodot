972d61e3092d26b9b10db72901c738ee2a37d139
nhaar <38634785+nhaar@users.noreply.github.com>
2024-01-11 20:46:29 -0300

    change collision method

--- Scenes/AstroBarrier/AstroBarrierGame.cs
+++ Scenes/AstroBarrier/AstroBarrierGame.cs
@@ -40,7 +40,7 @@ public partial class AstroBarrierGame : Node
 	}
 
 	/// <summary>
-	/// Checks for the collision of two objects with square hitboxes. Given positions are centered.
+	/// Checks for the collision of two objects with rectangle hitboxes. Given positions are centered.
 	/// </summary>
 	/// <param name="obj1"></param>
 	/// <param name="obj2"></param>
@@ -50,11 +50,20 @@ public partial class AstroBarrierGame : Node
 		Sprite2D[] objs = new[] { obj1, obj2 };
 		Vector2[] sizes = objs.Select(obj => obj.Texture.GetSize()).ToArray();
 		Vector2[] toplefts = objs.Select((obj, i) => obj.Position - sizes[i] / 2).ToArray();
+        Vector2[] bottomrights = objs.Select((obj, i) => obj.Position + sizes[i] / 2).ToArray();
 
-		return !(toplefts[0].X + sizes[0].X <= toplefts[1].X ||
-			toplefts[0].X >= toplefts[1].X + sizes[1].X ||
-			toplefts[0].Y + sizes[0].Y <= toplefts[1].Y ||
-			toplefts[0].Y >= toplefts[1].Y + sizes[1].Y);
+        // one object is to the left of the other
+        if (toplefts[0].X > bottomrights[1].X || bottomrights[0].X > toplefts[1].X)
+		{
+			return false;
+		}
+		// one object is above the other
+		if (toplefts[0].Y > bottomrights[1].Y || bottomrights[0].Y > toplefts[1].Y)
+		{
+            return false;
+        }
+
+		return true;
 	}
 
 	public override void _Ready()
