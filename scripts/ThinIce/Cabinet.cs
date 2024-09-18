056d9c9f476451b3db03798843d7c27cd3446b83
nhaar <38634785+nhaar@users.noreply.github.com>
2024-09-18 08:52:08 -0300

    change file organization

---
+++ scripts/ThinIce/Cabinet.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+namespace ClubPenguinPlus.ThinIce
+{
+	/// <summary>
+	/// Node for the Thin Ice Arcade Cabinet image.
+	/// </summary>
+	public partial class Cabinet : Sprite2D
+	{
+		public override void _Ready()
+		{
+			// the original doesn't do this, it uses a placeholder, here we are properly centering
+			// the game
+			// scale image to fit the height of the screen
+			Scale = new Vector2(1, 1) * 1080f / Texture.GetSize().Y;
+
+			// move the image to the center of the screen
+			Translate(new Vector2(1920f / 2, 1080f / 2));
+		}
+	}
+}
