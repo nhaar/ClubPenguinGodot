5da155cf960ffe7617cbf01742a51f59cd91e520
nhaar <38634785+nhaar@users.noreply.github.com>
2024-09-20 18:35:41 -0300

    refactor cabinet and keys scenes

--- scripts/ThinIce/SideCabinetKey.cs
+++ scripts/ThinIce/SideCabinetKey.cs
@@ -3,32 +3,32 @@ using System;
 
 namespace ClubPenguinPlus.ThinIce
 {
-    /// <summary>
-    /// Class for one of side cabinet keys.
-    /// </summary>
-    public partial class SideCabinetKey : CabinetKey
-    {
-        public static readonly float VerticalPosition = 2200f;
+	/// <summary>
+	/// Class for one of side cabinet keys.
+	/// </summary>
+	public partial class SideCabinetKey : CabinetKey
+	{
+		public static readonly float VerticalPosition = 2200f;
 
-        public static readonly float SidePosition = 2000f;
+		public static readonly float SidePosition = 2000f;
 
-        /// <summary>
-        /// Whether this is the left key or the right key
-        /// </summary>
-        [Export]
-        public bool IsLeft { get; set; }
+		/// <summary>
+		/// Whether this is the left key or the right key
+		/// </summary>
+		[Export]
+		public bool IsLeft { get; set; }
 
-        public float XPosition => SidePosition * (IsLeft ? -1 : 1);
+		public float XPosition => SidePosition * (IsLeft ? -1 : 1);
 
-        public override void _Ready()
-        {
-            if (IsLeft)
-            {
-                Scale = new Vector2(-1, 1);
-            }
-            Position = new Vector2(XPosition, VerticalPosition);
+		public override void _Ready()
+		{
+			if (IsLeft)
+			{
+				Scale = new Vector2(-1, 1);
+			}
+			Position = new Vector2(XPosition, VerticalPosition);
 
-            base._Ready();
-        }
-    }
+			base._Ready();
+		}
+	}
 }
