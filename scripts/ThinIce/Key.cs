0bec52a501c610e9035a989535e63657c68401e5
nhaar <38634785+nhaar@users.noreply.github.com>
2024-09-25 19:24:16 -0300

    add key animation

--- scripts/ThinIce/Key.cs
+++ scripts/ThinIce/Key.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using ClubPenguinPlus.Utils;
 
 namespace ClubPenguinPlus.ThinIce
 {
@@ -9,12 +10,21 @@ namespace ClubPenguinPlus.ThinIce
 	public partial class Key : Sprite2D
 	{
 		[Export]
-		public Texture2D KeyTexture { get; set; }
+		private SpriteFrames KeyAnimationFrames { get; set; }
+
+		private FramerateBoundAnimation KeyAnimation { get; set; }
 
 		public override void _Ready()
 		{
 			base._Ready();
-			Texture = KeyTexture;
+			KeyAnimation = new(KeyAnimationFrames, this);
+			KeyAnimation.Start();
+		}
+
+		public override void _Process(double delta)
+		{
+			base._Process(delta);
+			KeyAnimation.Advance();
 		}
 	}
 }
